2124475cebd7f83dace2bf142323be8e01c21508
TelKre <ruanjian82255@163.com>
2021-04-18 20:51:04 +0800

    第二次作业

---
+++ runner-core/src/main/java/com/youcheng/runner/core/service/impl/ExampleServiceImpl.java
@@ -0,0 +1,24 @@
+package com.youcheng.runner.core.service.impl;
+
+import com.youcheng.runner.core.service.domain.Example;
+import com.youcheng.runner.core.service.repository.ExampleRepository;
+import com.youcheng.runner.core.service.ExampleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExampleServiceImpl implements ExampleService{
+
+    @Autowired
+    private ExampleRepository exampleRepository;
+
+    @Override
+    public Example save(Example example) {
+        return exampleRepository.save(example);
+    }
+
+    @Override
+    public Example findById(Long id) {
+        return exampleRepository.findOne(id);
+    }
+}
---
+++ runner-core/src/main/java/com/youcheng/runner/core/service/repository/ExampleRepository.java
@@ -0,0 +1,7 @@
+package com.youcheng.runner.core.service.repository;
+
+import com.youcheng.runner.core.service.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExampleRepository extends JpaRepository<Example, Long>{
+}
